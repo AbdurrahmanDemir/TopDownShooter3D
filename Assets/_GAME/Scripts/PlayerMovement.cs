1776900f85c07a85f13bc605af80547faf080e7d
Abdurrahman <abdurrahmandmr11@gmail.com>
2024-03-12 16:11:09 +0300

    Walk

--- Assets/_GAME/Scripts/PlayerMovement.cs
+++ Assets/_GAME/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private PlayerControls playerControls;
     CharacterController characterController;
     public Vector3 movementDirection;
+    private Animator animator;
 
     public float walkSpeed;
     float verticalVelocity;
@@ -17,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
 
 
     [Header("Aim Info")]
+    public Transform aim;
     [SerializeField] private LayerMask aimLayerMask;
     private Vector3 lookingDirection;
 
@@ -33,16 +35,37 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator= GetComponentInChildren<Animator>();
     }
     private void Update()
     {
         ApplyMovement();
+        AimTowardsMouse();
+        AnimatorControllers();
+    }
 
+    private void AnimatorControllers()
+    {
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+
+        animator.SetFloat("xVelocity", xVelocity, .1f, Time.deltaTime);
+        animator.SetFloat("zVelocity", zVelocity, .1f, Time.deltaTime);
+    }
+
+    private void AimTowardsMouse()
+    {
         Ray ray = Camera.main.ScreenPointToRay(aimInput);
 
-        if(Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
         {
-            lookingDirection= hitInfo.point-transform.position;
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0f;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
         }
     }
 
