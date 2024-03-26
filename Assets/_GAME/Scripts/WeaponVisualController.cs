6a5c667e37cb77c3237545615d8787ff1d4e5d1e
Abdurrahman <abdurrahmandmr11@gmail.com>
2024-03-26 15:38:10 +0300

    weapon

--- Assets/_GAME/Scripts/WeaponVisualController.cs
+++ Assets/_GAME/Scripts/WeaponVisualController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WeaponVisualController : MonoBehaviour
 {
+    Animator anim;
     [SerializeField] private Transform[] guns;
 
     [SerializeField] private Transform pistol;
@@ -12,18 +13,41 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private void Start()
+    {
+        anim= GetComponentInParent<Animator>();
+        SwitchOn(pistol);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             SwitchOn(pistol);
+            SwichAnimationLayer(1);
+        }
         if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             SwitchOn(revolver);
+            SwichAnimationLayer(1);
+
+        }
         if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
             SwitchOn(autoRifle);
+            SwichAnimationLayer(1);
+
+        }
         if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
             SwitchOn(shotgun);
+            SwichAnimationLayer(2);
+        }
         if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
             SwitchOn(rifle);
+            SwichAnimationLayer(3);
+        }
 
     }
     void SwitchOn(Transform gunTransform)
@@ -37,5 +61,14 @@ public class WeaponVisualController : MonoBehaviour
         {
             guns[i].gameObject.SetActive(false);
         }
+    }
+    private void SwichAnimationLayer(int layerIndex)
+    {
+        for (int i = 1; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+
+        anim.SetLayerWeight(1, 0);
     }
 }
